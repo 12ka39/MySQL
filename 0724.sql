df0b1fcfdaaf507fbb699a81fcf0e9f3e1655ab8
12ka39 <work_lee2@naver.com>
2024-07-29 15:49:24 +0900

    0724 mysql

--- 0724.sql
+++ 0724.sql
@@ -1,71 +1,103 @@
+/*
+	Author: N
+    Date : 2024-07-24
+    Objective: Basic SELECT
+    Environment : Windows10, MySQL Workbench 8.0.38, MySQL 8.0.63
+    
+*/
+
 /* insert */
 
 use mycompany;
-select * from dept;
+SELECT * FROM dept;
 
 /* 이렇게 해도 되지만 다른 사람이 보면 가독성이 안 좋음 */
-insert into dept values (50, 'Design', 'Seoul');
+INSERT INTO dept 
+VALUES (50, 'Design', 'Busan');
 
-/* 컬럼명을 적어주면 해당 컬럼에 맞는 값만 잘 적어주면 순서는 상관 없음 */
-insert into dept(dname, loc, deptno)
-values ('Development', 'Daejun', 60);
+
+/* 컬럼 순서에 맞는 값만 잘 적어주면 컬럼 뭘 먼저 쓰든 상관 없음 */
+INSERT INTO dept(loc, deptno, dname)
+VALUES('Taejeon', 60, 'Development');
+
+
+/* 암시적 NULL */
+INSERT INTO dept(deptno, dname, loc)
+VALUES(70, NULL, 'Inchon');
+
 
 /* 명시적으로 NULL 을 쓸 때 */
 insert into dept(deptno, dname, loc) 
 values(70, null, 'Incheon');
 
 
-/* 암시적 NULL */
-insert into dept(deptno, loc) values(80, 'Busan');
+/* 제약조건 - 부서번호 deptno(PK)는 입력 필수 사항이라 아래 sql문은 오류다 */
+INSERT INTO dept(dname, loc)
+VALUES('Account', 'Yongin'); /* 오류 */
+
+/* 제약 조건 위배 - 부서번호(deptno)가 dept라는 부모 테이블에서 값을 참조(FK)하는데 
+부모테이블에 없는 값이라 오류 */
+INSERT INTO emp(empno, deptno)
+VALUES(9999, 90);
 
 
 
-/* 제약조건 - 부서명(PK)는 입력 필수 사항이라 아래 sql문은 오류다 */
-insert into dept(dname, loc) values ('Account', 'Yongin');
+/* 데이터 불일치. hiredate는 '연월일'만 나오고, now()는 '연월일 시분초'라. 타입이 안 맞음. 
+그래도 입력하면 warning 뜨면서 시분초 잘리고 데이터 들어가지만
+				==> curdate() 쓰자 */
+               
+/*  CURDATE(), 연월일 */
+INSERT INTO emp(empno, ename, hiredate, deptno)
+VALUES(9998, 'Jimin', CURDATE(), 40); 
 
-/* 제약 조건에 위배 - 부모 테이블에 없는 값이라 오류 ????? */
-insert into emp(empno, deptno)
-values(9999, 90);
+/* NOW() 연월일 시분초 */
+INSERT INTO emp(empno, ename, hiredate, deptno)
+VALUES(9990, 'Jimin', NOW(), 40); /* now 로 해도 경고 뜨고 데이터는 연월일까지만 잘려서 들어감*/
+
+SELECT * FROM emp;
 
 
+/* copy 하면 제약조건 다 날아간다 
+스키마(구조) + 데이터 복사 */
+CREATE TABLE emp_copy
+AS
+SELECT empno, ename, sal, hiredate
+FROM emp 
+WHERE deptno = 10;
 
-/* 데이터 불일치. hiredate는 날짜만 나오고, now()는 연원일 시분초라. 타입이 안 맞음. ==> cu */
-insert into emp(empno, ename, hiredate, deptno) 
-values(9999, 'Jimin', CURDATE(), 80); /* 이거 왜 오류남 >>>*/
---values(9999, 'Jimin', NOW(), 80);
 
-
-/* copy 하면 제약조건 다 날아간다 */
-create table emp_copy
-as
-select empno, ename, sal, hiredate
-from emp 
-where deptno = 10;
-
-/* 스키마만 복사하고 데이터는 복사하지 않는 방법 */
-create table emp_copy1
-as
-select *
-from emp 
-where 0 = 1; /* 이런 조건을 충족하는 데이터는 없으니까 
+/* 스키마(구조)만 복사 - WHERE 절에 말도 안 되는 조건 줘서 데이터는 없게 만든다. */
+CREATE TABLE emp_copy1
+AS
+SELECT *
+FROM emp 
+WHERE 0 = 1; /* 이런 조건을 충족하는 데이터는 없으니까 
 데이터는 제외하고, 구조만 복사된다. */
 
 
-select * from emp_copy1; /* 스키마만 복사한 테이블 */
+SELECT * FROM emp_copy1; /* 스키마만 복사한 테이블 - 제약조건 초기화됨 */
 /* 복사한 테이블에 데이터 추가 (한글이 되는지 확인하자) */
-insert into emp_copy1(empno, ename) values (1122, "한지민");
+
+INSERT INTO emp_copy1(empno, ename) VALUES (1122, "한지민");
 
 
-/* update 와 delete는 반드사 where이 있어야 한다. 안 그러면 전체 적용됨. */
+/* update 와 delete는 반드사 where이 있어야 한다. 
+						안 그러면 전체 적용되니 주의하자. */
 
-/* update    set   */
-select * from dept;
+/* UPDATE      SET      */
+SELECT * FROM dept;
 
-update dept 
-set dname='finance'
-where deptno=70; 
+INSERT INTO dept(deptno) VALUES (70);
+
+UPDATE dept
+SET dname = 'FINANCE'
+WHERE deptno = 70;
 
 /* 여러 개를 한꺼번에 바꿀 때 */
-update dept 
-set dname='HR', loc='Busan'
-where deptno=70; 
+UPDATE dept 
+SET dname='HR', loc='Busan'
+WHERE deptno=70; 
+
+UPDATE DEPT 
+SET DEPTNO=60, DNAME='MI', LOC='SKY'
+WHERE DEPTNO=70;
--- 0724_sungjuk.sql
+++ 0724_sungjuk.sql
@@ -1,3 +1,9 @@
+/*
+JAVA 성적관리 프로그램 학생 테이블
+https://github.com/12ka39/Java_Core/tree/main/0724%EC%84%B1%EC%A0%81%EA%B4%80%EB%A6%AC%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A82
+*/
+
+
 CREATE TABLE Student(
 hakbun CHAR(4)   PRIMARY KEY,
 name VARCHAR(20) NOT NULL, 
