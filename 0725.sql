ebcee57b00511f7dc42953e9c8aa27f1110cdbfb
12ka39 <work_lee2@naver.com>
2024-07-29 18:11:44 +0900

    constraint

--- 0725.sql
+++ 0725.sql
@@ -12,21 +12,43 @@ CREATE TABLE Patient(
 );
 
 
-START TRANSACTION;
+/*
+	Author: N
+    Date : 2024-07-25
+    Objective: Basic SELECT
+    Environment : Windows10, MySQL Workbench 8.0.38, MySQL 8.0.63
+    
+*/
+
+/* 트랜젝션 한 개 이상의 sql문장 (DML) 집합, (복원 가능) */
+/* 순서
+
+START TRANSACTION;  // 이거 안 써주면 오류 롤백 안 됨
+SAVEPOINT aaa;
+ROLLBACK TO aaa;
+
+*/
+
+START TRANSACTION; 
 
 SELECT * FROM emp;
 
 SELECT * FROM emp
 WHERE empno=7782;
 
-UPDATE emp SET deptno=10 WHERE empno=7782;
-SAVEPOINT aaa; /* 세이브 포인트  ????????? */
+UPDATE emp 
+SET deptno=30
+WHERE empno=7782;
+
+
+SAVEPOINT aaa; /* 세이브 포인트 */
 
 
 INSERT INTO emp (empno,ename, job,mgr,hiredate,sal, comm, deptno)
-VALUES(7999, 'TOM', 'SALESMAN',7782,CURDATE(),2000,2000,10);
+VALUES(7997, 'TOM', 'SALESMAN',7782,CURDATE(),2000,2000,10);
 
-ROLLBACK TO aaa; /* 왜 안 됨?????? */
+ROLLBACK TO aaa; /* 세이브 포인트로 롤백 */
+
 
 
 /* 테이블 구조 변경  -- 근데 안 하는게 좋다 (오라클은 아예 구조 변경 막아둠) */
@@ -42,9 +64,10 @@ SELECT * FROM emp20;
 
 
 
-/* 2) alter table */
+/* 2) ALTER TABLE  - ADD, DROP, MODIFY */
 ALTER TABLE emp20
-ADD age TINYINT AFTER ename;
+ADD age TINYINT AFTER ename; /* 컬럼 위치 지정 */
+
 
 ALTER TABLE emp20
 DROP COLUMN sal;
@@ -53,7 +76,7 @@ ALTER TABLE emp20
 MODIFY ename VARCHAR(20);
 
 
-/* 3) drop table */
+/* 3) DROP TABLE */
 DROP TABLE emp20;
 
 
@@ -66,9 +89,10 @@ CREATE TABLE jusorok(
 
 
 INSERT INTO jusorok VALUE(1, DEFAULT);
-INSERT INTO jusorok VALUE(2, 'male');
+INSERT INTO jusorok VALUE(2, 'MALE');
 
-select * from jusorok;
+SELECT * FROM jusorok;
+
 
 
 CREATE TABLE Student(
@@ -77,7 +101,7 @@ CREATE TABLE Student(
     kor      TINYINT     NOT NULL CHECK(kor BETWEEN 0 AND 100),
     eng      TINYINT     NOT NULL,
     mat      TINYINT     NOT NULL DEFAULT 0,
-    edp      TINYINT,  /* 여기 not null을 밑에서 추가 */
+    edp      TINYINT,  /* 테이블 생성 후 not null을 추가해봤다 */
     tot      SMALLINT,
     avg      FLOAT(5,2),
     grade    CHAR(1),
@@ -87,13 +111,13 @@ CREATE TABLE Student(
     CONSTRAINT student_name_uk  UNIQUE(name),
     CONSTRAINT student_grade_ck CHECK(grade IN('A','B','C','D','F')),
     CONSTRAINT student_deptno_fk FOREIGN KEY(deptno)
-    REFERENCES dept(deptno)
+			REFERENCES dept(deptno)
 
 ) DEFAULT CHARSET utf8;
 
 
 ALTER TABLE Student
-MODIFY edp TINYINT NOT NULL;
+MODIFY tot TINYINT NOT NULL;
 
 
 /* 테이블 레벨      제약 조건 추가 -> ADD CONSTRAINT */
